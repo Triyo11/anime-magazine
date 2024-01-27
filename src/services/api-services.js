a6beca3d7335d1b5e729a2da95933ae2b588afad
Triyo11 <rusdiantoro117@gmail.com>
2024-01-27 21:44:01 +0700

    modify getNestedAnimeResponse

--- src/services/api-services.js
+++ src/services/api-services.js
@@ -1,21 +1,21 @@
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 
 dotenv.config();
 
-const api_anime = "https://api.jikan.moe/v4"
+const api_anime = "https://api.jikan.moe/v4";
 console.log(api_anime);
 
 const getAnimeResponse = async (resource, query) => {
-  const response = await fetch(
-    `${api_anime}/${resource}?${query}`
-  );
+  const response = await fetch(`${api_anime}/${resource}?${query}`);
   const anime = await response.json();
   return anime;
 };
 
 const getNestedAnimeResponse = async (resource, objectProperty) => {
-  const response = await getAnimeResponse(resource);
-  const recommendedAnime = response?.data.flatMap(
+  // const response = await getAnimeResponse(resource);
+  const response = await fetch(`${api_anime}/${resource}`);
+  const anime = await response.json();
+  const recommendedAnime = anime.data.flatMap(
     (item) => item[objectProperty]
   );
   return recommendedAnime;
@@ -26,4 +26,8 @@ const reproduceRecommendedAnime = (data, gap) => {
   return { data: data.slice(startIndexAnime, startIndexAnime + gap) };
 };
 
-module.exports = {getAnimeResponse, getNestedAnimeResponse, reproduceRecommendedAnime}
+module.exports = {
+  getAnimeResponse,
+  getNestedAnimeResponse,
+  reproduceRecommendedAnime,
+};
