75517574d6907d7a72179905e84cf568d23aeac4
Triyo11 <rusdiantoro117@gmail.com>
2024-01-27 18:03:30 +0700

    fix dotenv

--- src/services/api-services.js
+++ src/services/api-services.js
@@ -1,16 +1,24 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const getAnimeResponse = async (resource, query) => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`)
-  const anime = await response.json()
-  return anime
-}
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`
+  );
+  const anime = await response.json();
+  return anime;
+};
 
 export const getNestedAnimeResponse = async (resource, objectProperty) => {
-  const response = await getAnimeResponse(resource)
-  const recommendedAnime = response.data.flatMap(item => item[objectProperty])
-  return recommendedAnime
-}
+  const response = await getAnimeResponse(resource);
+  const recommendedAnime = response.data.flatMap(
+    (item) => item[objectProperty]
+  );
+  return recommendedAnime;
+};
 
 export const reproduceRecommendedAnime = (data, gap) => {
-  const startIndexAnime = Math.floor(Math.random() * ((data.length - gap) + 1))
-  return {data : data.slice(startIndexAnime, startIndexAnime + gap)}
-} 
+  const startIndexAnime = Math.floor(Math.random() * (data.length - gap + 1));
+  return { data: data.slice(startIndexAnime, startIndexAnime + gap) };
+};
